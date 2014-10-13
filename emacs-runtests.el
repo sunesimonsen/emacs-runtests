82203904bd0354cdbda34860f2573cd3450144da
Sune Simonsen <sune@we-knowhow.dk>
2014-10-13 09:39:43 +0200

    Added a copyright and change some minor things

--- emacs-runtests.el
+++ emacs-runtests.el
@@ -1,3 +1,22 @@
+;;; emacs-runtests.el --- Run unit tests from Emacs
+;;
+;; Copyright 2014 Sune Simonsen
+;;
+;; Author: Sune Simonsen <sune@we-knowhow.dk>
+;; URL: https://github.com/sunesimonsen/emacs-runtests
+;; Keywords: test
+;; Version: DEV
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 2 of
+;; the License, or (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be
+;; useful, but WITHOUT ANY WARRANTY; without even the implied
+;; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+;; PURPOSE.  See the GNU General Public License for more details.
+
 (require 'ansi-color)
 
 (defun runtests ()
@@ -29,5 +48,6 @@
   (set-face-background 'mode-line color))
 
 (defun runtests--color-buffer ()
-  (interactive)
   (ansi-color-apply-on-region (point-min) (point-max)))
+
+(provide 'runtests)
